3db93b5bffcf1a3ddb792bb9b8a540c4126f4a3d
Gianluca <ggeraci.123@gmail.com>
2025-12-01 16:56:39 +0100

    add debug script

---
+++ debug.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p dev_accelerated
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16G
+#SBATCH --time=00:16:00  #40.000 steps should take ~6:45h
+#SBATCH -J debug_flower
+#SBATCH -o logs/%x_%j.out
+#SBATCH -e logs/%x_%j.err
+
+source ~/.bashrc
+conda activate lerobot-irl-models
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export TOKENIZERS_PARALLELISM=false
+export HYDRA_FULL_ERROR=1
+
+# Start training
+python src/train_flower.py wandb.enable=false dataset.repo_id=test dataset.dataset_path=/hkfs/work/workspace/scratch/usmrd-MemVLA/datasets/lerobot/test train.steps=1000 train.save_freq=1000
