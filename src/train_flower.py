3e20356c70f43a6b98217f887255a5f71bbae39e
Gianluca <ggeraci.123@gmail.com>
2025-12-06 10:24:31 +0100

    enforce loading of checkpoints

--- src/train_flower.py
+++ src/train_flower.py
@@ -11,8 +11,9 @@ from lerobot.configs.default import DatasetConfig, WandBConfig
 from lerobot.configs.train import TrainPipelineConfig
 from lerobot.policies import factory
 
-from src.policies.flower.processor_flower import make_flower_pre_post_processors
 from src.policies.flower.flower_config import FlowerVLAConfig
+from src.policies.flower.flower_policymaker import make_flower_policy
+from src.policies.flower.processor_flower import make_flower_pre_post_processors
 
 
 def my_make_pre_post_processors(policy_cfg, pretrained_path=None, **kwargs):
@@ -26,6 +27,7 @@ def my_make_pre_post_processors(policy_cfg, pretrained_path=None, **kwargs):
 
 # _original_make_pre_post_processors = factory.make_pre_post_processors
 factory.make_pre_post_processors = my_make_pre_post_processors
+factory.make_policy = make_flower_policy  # monkey patch custom policy maker to pass pretrained non lerobot models
 
 from lerobot.scripts.lerobot_train import train as lerobot_train
 from lerobot.utils.utils import init_logging
@@ -39,56 +41,6 @@ sys.path.insert(0, str(project_root))
 log = logging.getLogger(__name__)
 
 
-def load_flower_policy(pretrained_config, checkpoint_path):
-    """
-    This method loads the FLowerVLAPolicy class and handles its initialization from passed checkpoint
-    that can either be from flower_vla_pret or checkpoints of previous flower versions trained with lerobot (resume = True)
-    """
-    policy = FlowerVLAPolicy(pretrained_config)
-    if checkpoint_path:
-        checkpoint = torch.load(
-            checkpoint_path,
-            map_location="cpu",
-        )
-
-        # Handle different checkpoint formats
-        if isinstance(checkpoint, dict):
-            if "model_state_dict" in checkpoint:
-                state_dict = checkpoint["model_state_dict"]
-            elif "state_dict" in checkpoint:
-                state_dict = checkpoint["state_dict"]
-            elif "model" in checkpoint:
-                state_dict = checkpoint["model"]
-            else:
-                state_dict = checkpoint
-        else:
-            state_dict = checkpoint  # NOTE: for flower we use this line
-
-        new_state_dict = {}
-        for key, value in state_dict.items():
-            new_key = key
-            if new_key.startswith("agent."):
-                new_key = "model." + new_key[6:]
-            new_key = new_key.replace(".mlp.c_fc1.", ".mlp.fc1.")
-            new_key = new_key.replace(".mlp.c_fc2.", ".mlp.fc2.")
-            new_key = new_key.replace(".mlp.c_proj.", ".mlp.proj.")
-
-            new_state_dict[new_key] = value
-
-        state_dict = new_state_dict
-        missing_keys, unexpected_keys = policy.load_state_dict(state_dict, strict=False)
-
-        if missing_keys:
-            log.warning(f"Missing keys when loading checkpoint: {missing_keys}")
-        if unexpected_keys:
-            log.warning(f"Unexpected keys when loading checkpoint: {unexpected_keys}")
-
-        log.info(f"Sucessfully loaded pretrained weights from {checkpoint_path}!")
-    else:
-        log.info(f"No pretrained weights provided, training from scratch")
-    return policy
-
-
 @hydra.main(
     config_path="../configs/flower", config_name="flower_config", version_base="1.3"
 )
@@ -98,10 +50,21 @@ def train(cfg):
         root=cfg.dataset_path,
         video_backend="pyav",
     )
-    pretrained_config = hydra.utils.instantiate(cfg.model, _convert_="all")
-    pretrained_config.device = "cpu"  # set cpu for model loading, otherwise OOM Error
-    policy = load_flower_policy(pretrained_config, cfg.checkpoint_path)
+
+    # TODO: make sure that correct device types are set to avoid OOM errors when instantiating the policy
+    # and passing it to the accelerator
+    if cfg.train.resume:
+        pretrained_config = FlowerVLAConfig.from_pretrained(
+            pretrained_name_or_path=cfg.checkpoint_path
+        )
+        pretrained_config.device = (
+            "cpu"  # Currently there seems no better way of enforcing this
+        )
+
+    else:
+        pretrained_config = hydra.utils.instantiate(cfg.model, _convert_="all")
     pretrained_config.device = "cuda"
+    pretrained_config.pretrained_path = cfg.checkpoint_path
 
     train_cfg = TrainPipelineConfig(
         policy=pretrained_config,
@@ -121,8 +84,6 @@ def train(cfg):
             mode=cfg.wandb.mode,
         ),
     )
-
-    train_cfg.pretrained_policy = policy
 
     init_logging()
     lerobot_train(train_cfg)
