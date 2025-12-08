c49833ed71619b224c74870b9174f3a9327baca8
Gianluca <ggeraci.123@gmail.com>
2025-12-08 12:18:31 +0100

    adapt training scripts

--- debug_xvla.sh
+++ debug_xvla.sh
@@ -35,7 +35,7 @@ echo "Batch size per GPU: $BATCH_PER_GPU"
 echo "Launching on GPUs: $SLURM_JOB_GPUS"
 
 now=$(date +%Y-%m-%d/%H-%M-%S)
-OUTPUT_DIR="./output/train/flower/${now}"
+OUTPUT_DIR="./output/debug/flower/${now}"
 # ==============================================================================
 # EXECUTE TRAINING (Conditional Launch)
 # ==============================================================================
@@ -46,13 +46,19 @@ if [ "$NUM_GPUS" -gt 1 ]; then
         --num_processes=$NUM_GPUS \
         --main_process_port $MASTER_PORT \
         src/train_xvla.py \
-        wandb.enable=False \
-        train.steps=500 \
-        train.batch_size=$BATCH_PER_GPU
+        wandb.enable=True \
+        train.steps=1000 \
+        train.batch_size=$BATCH_PER_GPU \
+        train.output_dir=$OUTPUT_DIR \
+        policy.pretrained_path="/home/hk-project-p0024638/usmrd/projects/lerobot-irl-models/output/train/xvla/2025-12-07/17-56-59/model_outputs/checkpoints/020000/pretrained_model"
+        # policy.action_mode="custom_joint8"  \
+        # repo_id="test" \
+        # dataset_path="/hkfs/work/workspace/scratch/usmrd-MemVLA/datasets/lerobot/test" \
 else
     # Single-GPU (or CPU) training using standard python call
     python src/train_xvla.py \
         wandb.enable=False \
         train.steps=500 \
-        train.batch_size=$BATCH_PER_GPU
+        train.batch_size=$BATCH_PER_GPU \
+        train.output_dir=$OUTPUT_DIR
 fi
